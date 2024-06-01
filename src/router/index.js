abd6fffdd96657384347d4d0a770589df220ad1e
ViniciusSantAzambuja <viniciussantana.azambuj4@gmail.com>
2024-06-01 14:06:11 -0400

    adicionado tela de match, falta colocar os dados de usuario nela

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ProfileDetailsView from '@/views/ProfileDetailsView.vue'
 import IamView from "@/views/IamView.vue"
 import passionsView from "@/views/passionsView.vue"
 import MainView from '@/views/MainView.vue'
+import MatchView from '@/views/MatchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +45,11 @@ const router = createRouter({
       path: '/passions',
       name: 'passions',
       component: passionsView
+    },
+    {
+      path: '/match',
+      name: 'match',
+      component: MatchView
     }
   ]
 })
